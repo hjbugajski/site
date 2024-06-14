4ea9a89b3574296ad21c47907923fcbc69e4ab0a
Henry Bugajski <henry.bug.dev@gmail.com>
2024-06-14 18:57:04 -0400

    chore: update dependencies (#10)
    * chore: update dependencies
    
    * refactor: remove unnecessary imports

--- src/payload/components/row-label/index.tsx
+++ src/payload/components/row-label/index.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import React from 'react';
-
 import { useRowLabel } from '@payloadcms/ui/forms/RowLabel/Context';
 import { Data } from 'payload/types';
 
@@ -9,5 +7,5 @@ export function RowLabel({ path, fallback }: { path: string; fallback: string })
   const { data, rowNumber } = useRowLabel<Data>();
   const fieldValue: any = path.split('.').reduce((value, part) => value?.[part], data);
 
-  return <span>{fieldValue || `${fallback} ${rowNumber}`}</span>;
+  return <>{fieldValue || `${fallback} ${rowNumber}`}</>;
 }
