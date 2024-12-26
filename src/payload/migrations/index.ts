36a5f8db89c5049d455110d6466fe2efbef510a6
Henry Bugajski <henry.bug.dev@gmail.com>
2024-12-26 09:47:07 -0600

    create migrations

---
+++ src/payload/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241226_154624 from './20241226_154624';
+
+export const migrations = [
+  {
+    up: migration_20241226_154624.up,
+    down: migration_20241226_154624.down,
+    name: '20241226_154624'
+  },
+];
