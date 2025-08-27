b2b9635747e68e9dac0d0477262c0c0f782ee192
Henry Bugajski <henry.bug.dev@gmail.com>
2025-08-27 17:09:24 -0400

    chore: update dependencies

--- src/payload/migrations/index.ts
+++ src/payload/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20241226_154624 from './20241226_154624';
+import * as migration_20250827_153358 from './20250827_153358';
 
 export const migrations = [
   {
     up: migration_20241226_154624.up,
     down: migration_20241226_154624.down,
-    name: '20241226_154624'
+    name: '20241226_154624',
+  },
+  {
+    up: migration_20250827_153358.up,
+    down: migration_20250827_153358.down,
+    name: '20250827_153358'
   },
 ];
